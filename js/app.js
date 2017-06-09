7dfa8d7a903f66127c20c782c21b771b19fddbf6
ace_e303 <ace_e303@e303-s10.lab.lagcc>
2017-06-09 16:45:02 -0400

    added close funtionality

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 var userSubmit = document.querySelector('#user-submit');
+var toDoList = document.querySelector('#to-do-list');
 
 function addToDo(event){
   event.preventDefault();
 
   var userInput = document.querySelector('#user-input');
-  var toDoList = document.querySelector('#to-do-list');
 
   if(userInput.value === ''){
     return false;
   }
 
   // toDoList.innerHTML += '<li>' + userInput.value + '</li>';
-  toDoList.innerHTML = '<li>' + userInput.value + '</li>' + toDoList.innerHTML;
+  toDoList.innerHTML = '<li>' + userInput.value + '<i class="fa fa-window-close close-to-do" aria-hidden="true"></i></li>' + toDoList.innerHTML;
 
   userInput.value = '';
 }
 
+function removeToDo(event) {
+  if (event.target.classList.contains('close-to-do')){
+    var li = event.target.parentElement;
+    toDoList.removeChild(li);
+  }
+}
+
+
 userSubmit.addEventListener('click', addToDo, false);
+toDoList.addEventListener('click', removeToDo, false);
